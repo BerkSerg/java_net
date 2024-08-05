c265bdc0507eb40b580fda04cca220d222f371b3
BerkSerg <berkserg@bk.ru>
2024-08-06 00:05:09 +0400

    HW1-2

--- lesson1/src/clientServer/Client.java
+++ lesson1/src/clientServer/Client.java
@@ -1,4 +1,58 @@
 package clientServer;
 
+import java.io.*;
+import java.net.InetAddress;
+import java.net.Socket;
+
 public class Client {
+    private static final int SERVER_PORT = 5555;
+    private static final String SERVER_HOST = "localhost";
+
+    public static void main(String[] args) {
+        Socket socket = null;
+
+        try{
+            try{
+                System.out.printf("Добро пожаловать на клиентскую сторону \nПодключение к серверу\n\t (IP адрес %s, порт %s)", SERVER_HOST, SERVER_PORT);
+                InetAddress address = InetAddress.getByName(SERVER_HOST);
+                socket = new Socket(address, SERVER_PORT);
+                System.out.println("\nСоединение установлено");
+                System.out.println("\t Адрес хоста " + socket.getInetAddress().getHostAddress() + "\t Размер буфера " + socket.getReceiveBufferSize());
+
+                DataInputStream dis = new DataInputStream(socket.getInputStream());
+                DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+
+                InputStreamReader isr = new InputStreamReader(System.in);
+                BufferedReader br = new BufferedReader(isr);
+                System.out.println("Хотите получить цитату дня (yes/no)?");
+                String line = br.readLine();
+                while (!line.isEmpty()){
+                    if(line.equalsIgnoreCase("yes") || line.equalsIgnoreCase("no")){
+                        dos.writeUTF(line);
+                        dos.flush();
+
+                        line = dis.readUTF();
+                        if(line.endsWith("quit")){
+                            System.out.println("Спасибо за использование сервиса");
+                            break;
+                        }
+
+                        System.out.println("\nСервер отправил цитату: \n\t" + line + "\n");
+                    }
+                    System.out.println("Хотите получить цитату дня?");
+                    line = br.readLine();
+                }
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+        } finally{
+            try {
+                if (socket != null) {
+                   socket.close();
+                }
+            } catch (IOException e) {
+                    e.printStackTrace();
+            }
+        }
+    }
 }
--- lesson1/src/clientServer/Server.java
+++ lesson1/src/clientServer/Server.java
@@ -1,4 +1,89 @@
 package clientServer;
 
-public class Server {
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server extends Thread {
+    private static final int PORT = 5555;
+    private String message = "Клиент %d отправил сообщение\n\t ";
+    private String connection = "Клиент %d закрыл соединение";
+
+    private Socket socket;
+    private int num;
+
+    public void setSocket(int num, Socket socket){
+        this.num = num;
+        this.socket = socket;
+        start();
+    }
+
+    @Override
+    public void run() {
+        try {
+            DataInputStream dis = new DataInputStream(socket.getInputStream());
+            DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+
+            String line = "";
+            boolean getNextQuote = false;
+            while (true){
+                line = dis.readUTF();
+
+                System.out.printf(message, num);
+                System.out.println(line);
+                System.out.println("Отправляю обратно...");
+
+                getNextQuote = line.equalsIgnoreCase("yes");
+
+                if(getNextQuote){
+                    dos.writeUTF(Quote.getRandomQuote());
+                } else {
+                    dos.writeUTF("quit");
+                }
+                dos.flush();
+
+                System.out.println();
+                if (!getNextQuote){
+                    socket.close();
+                    System.out.printf(connection, num);
+                    break;
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        ServerSocket serverSocket = null;
+
+        try {
+            try {
+                int i = 0;
+                InetAddress inetAddress = InetAddress.getByName("localhost");
+                serverSocket = new ServerSocket(PORT, 0, inetAddress);
+                System.out.println("Сервер запущен");
+                while (true) {
+                    Socket addSocket = serverSocket.accept();
+                    System.err.println("\n\n Клиент принят");
+
+                    new Server().setSocket(i++, addSocket);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } finally {
+            try {
+                if (serverSocket != null) {
+                    serverSocket.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
 }
