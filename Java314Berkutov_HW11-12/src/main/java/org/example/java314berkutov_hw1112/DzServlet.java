1958bab8dee33324f94717dc48570f0844a4e35f
BerkSerg <berkserg@bk.ru>
2024-09-10 22:00:21 +0400

    hw 11-12

---
+++ Java314Berkutov_HW11-12/src/main/java/org/example/java314berkutov_hw1112/DzServlet.java
@@ -0,0 +1,19 @@
+package org.example.java314berkutov_hw1112;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/dz")
+public class DzServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+        out.println("this is string from Dz servlet");
+    }
+}
